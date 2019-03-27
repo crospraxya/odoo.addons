43fcf47bdf97c08e6892db027e198d79633951b9
crospraxya <carlosros070612@gmail.com>
2019-03-27 11:55:42 +0100

    Inicio del modulo

---
+++ library/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Manager",
+
+    'summary': """
+        Library Manager
+        """,
+
+    'author': "Carlos",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Tools',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    'installable': True,
+}
