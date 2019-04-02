3a7726eca7ffa8d9f3687b3788e3c668d702df17
crospraxya <cros@praxya.com>
2019-04-02 18:02:53 +0200

    Qweb crear Report

---
+++ report_invoice_test/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Report Invoice Test",
+
+    'summary': """
+        Report Invoice Test""",
+
+
+    'author': "Carlos",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tools',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'report/reports.xml',
+        'report/account_invoice_report.xml'
+    ],
+    # only loaded in demonstration mode
+    'installable':True,
+}
