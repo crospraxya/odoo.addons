c95f3f0332344495b6f060077f234e4123fc7d7f
crospraxya <cros@praxya.com>
2019-04-03 11:09:36 +0200

    Mejorando reports

--- report_invoice_test/__manifest__.py
+++ report_invoice_test/__manifest__.py
@@ -16,13 +16,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['account'],
 
     # always loaded
     'data': [
+        'data/paperformat.xml',
         'report/reports.xml',
-        'report/account_invoice_report.xml'
+        'report/account_invoice_report.xml',
     ],
     # only loaded in demonstration mode
-    'installable':True,
+    'installable': True,
 }
